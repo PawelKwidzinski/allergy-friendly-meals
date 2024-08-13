5dd83bf411bc254bbd5c44fb5a51dd2756a00ddc
PawelKwidzinski <solvap83@gmail.com>
2024-08-13 10:41:41 +0200

    HistamineFoodApplication - Initial Commit

---
+++ histamine-food/src/main/java/pl/kwidzinski/histaminefood/HistamineFoodApplication.java
@@ -0,0 +1,13 @@
+package pl.kwidzinski.histaminefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistamineFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistamineFoodApplication.class, args);
+    }
+
+}
---
+++ histamine-food/src/test/java/pl/kwidzinski/histaminefood/HistamineFoodApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.kwidzinski.histaminefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistamineFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+        throw new UnsupportedOperationException("notImplemented() yet ...");
+    }
+
+}
