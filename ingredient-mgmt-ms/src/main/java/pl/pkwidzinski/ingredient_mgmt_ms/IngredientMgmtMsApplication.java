b84eb54b7b75c2a2324cc02c5c79527a096f3aef
pawelkwidzinski <solvap83@gmail.com>
2024-08-23 13:30:13 +0200

    (chore): initial commit for ingredient-mgmt-ms Refs: #7

---
+++ ingredient-mgmt-ms/src/main/java/pl/pkwidzinski/ingredient_mgmt_ms/IngredientMgmtMsApplication.java
@@ -0,0 +1,13 @@
+package pl.pkwidzinski.ingredient_mgmt_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientMgmtMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredientMgmtMsApplication.class, args);
+	}
+
+}
